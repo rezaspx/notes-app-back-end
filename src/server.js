3eb56aaf50d68ea44b1ea9be5b798551ec9e5290
Reza <rezaarbani12@gmail.com>
2022-05-04 09:24:38 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,41 +1,49 @@
 /* eslint-disable camelcase */
 
+// mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-// const routes = require('./routes'); tidak di pake lagi
+
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
-// const NotesService = require('./services/inMemory/NotesService');
 const NotesValidator = require('./validator/notes');
 
-// const notesPlugin = require('./notesPlugin');
-// const otherPlugin = require('./otherPlugin');
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
-    // dibawah host sebelumnya
-    // port: 5000,
-    // host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
       },
     },
   });
-
-  // server.route(routes); tidak di pake lagi
-
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
